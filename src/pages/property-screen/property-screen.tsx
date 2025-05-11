58d6673fc275d7eaee0ab3a0081aea8b549b5130
KomisAnt <skomorokh951@gmail.com>
2025-05-11 15:40:16 +0300

    change src avatarUrl in property-screen.tsx

--- src/pages/property-screen/property-screen.tsx
+++ src/pages/property-screen/property-screen.tsx
@@ -150,7 +150,7 @@ function PropertyScreen(): JSX.Element {
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
                   <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src={`{basename}/${room.host.avatarUrl}`} width="74" height="74" alt={room.host.name} />
+                    <img className="property__avatar user__avatar" src={room.host.avatarUrl} width="74" height="74" alt={room.host.name} />
                   </div>
                   <span className="property__user-name">
                     {room.host.name}
